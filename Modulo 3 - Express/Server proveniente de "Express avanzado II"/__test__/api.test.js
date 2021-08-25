85b91bd0b5750da96bad1668e848b587888fddef
spancavil <soancavil@gmail.com>
2021-08-25 12:04:43 -0300

    Finalizacion de los Test de la API-rest - TODO queda lanzar error 500

--- Modulo 3 - Express/Server proveniente de "Express avanzado II"/__test__/api.test.js
+++ Modulo 3 - Express/Server proveniente de "Express avanzado II"/__test__/api.test.js
@@ -1,8 +1,6 @@
 const request = require('supertest');
-const app = require('../app'); //importamos el server donde está centralizada nuestra app
+const app = require('../app'); //importamos nuestra app
 //Separamos el server de la app, para trabajarla por separado.
-
-const { nanoid } = require('nanoid');
 
 //Antes de todas las pruebas, arracamos la app
 beforeAll(() => {
@@ -22,7 +20,6 @@ describe('Get all products from /api/productos', () => {
 
         expect(response.status).toBe(200);
 
-        console.log(response.body);
 
     })
 })
@@ -39,6 +36,21 @@ describe('Save a product /api/productos', () => {
                     foto: "/url/ficticia"
                 }
             )
+
+        expect(response.status).toBe(200)
+        expect(response.body).not.toBeNull();
+    })
+
+    test('Should save a product', async () => {
+        const response = await request(app)
+            .post('/api/productos/guardar')
+            .send(
+                {
+                    nombre: "Producto 02",
+                    precio: 50000,
+                    foto: "/url/ficticia/02"
+                }
+            )
         console.log(response.body);
 
         expect(response.status).toBe(200)
@@ -51,8 +63,79 @@ describe('Get a specific product /api/productos', () => {
     test('Get a product with id = 1', async () => {
         const response = await request(app)
             .get('/api/productos/listar/1')
-        
+
         expect(response.status).toBe(200);
         expect(response.body.id).toBe(1);
     })
+
+    test('Get a nonexistent product with id = 3', async () => {
+        const response = await request(app)
+            .get('/api/productos/listar/3')
+
+        expect(response.status).toBe(200);
+        expect(response.body.error).not.toBeUndefined();
+    })
 })
+
+describe('Update a specific producto /api/productos/actualizar/:id', () => {
+
+    test('Update a producto with id =  1', async () => {
+        const response = await request(app)
+            .put('/api/productos/actualizar/1')
+            .send({
+                nombre: "Producto 01 modificado",
+                precio: 40000,
+                foto: "/url/ficticia/nro2"
+            })
+
+        expect(response.status).toBe(200);
+        expect(response.body.id).toBe(1);
+
+    })
+
+    test('Update a nonexistent product with id =  3', async () => {
+        const response = await request(app)
+            .put('/api/productos/actualizar/3')
+            .send({
+                nombre: "Producto 03 modificado",
+                precio: 40000,
+                foto: "/url/ficticia/nro3"
+            })
+
+        expect(response.status).toBe(200);
+        expect(response.body.id).toBeUndefined();
+        expect(response.body.error).not.toBeUndefined();
+    })
+})
+
+describe('Delete products /api/productos/borrar', () => {
+    test('Delete a product with id = 1', async () => {
+        const response = await request(app).delete('/api/productos/borrar/1');
+        expect(response.status).toBe(200);
+        expect(response.body.id).toBe(1);
+    })
+
+    test('Delete a nonexistent product with id = 3', async () => {
+        const response = await request(app).delete('/api/productos/borrar/3');
+        expect(response.status).toBe(200);
+        expect(response.body.error).not.toBeUndefined();
+    })
+})
+
+describe('Get all resultant products /api/productos/listar', () => {
+
+    //Test utilizando supertest directamente, a modo de ejemplo. No hace falta el async await, ya que esto ya es una promise.
+    test('Get all products with supertest only', (done) => {
+        request(app)
+            .get('/api/productos/listar')
+            .expect(200, done)
+    })
+
+    test('Get all products, should be 1', async () => {
+        const response = await request(app).get('/api/productos/listar')
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1)
+    })
+})
+
+//TODO al parecer para lanzar un error 500 hay que hacer un stub con Sinon (?) no netendí demasiado.
